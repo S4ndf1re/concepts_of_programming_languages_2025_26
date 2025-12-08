50be2178f56dbe84259db920ef3ef8272ccba101
Jan Kleinmann <jan.kleinmann@proton.me>
2025-12-08 20:15:37 +0100

    added slidev

---
+++ presentations/jit/global-bottom.vue
@@ -0,0 +1,13 @@
+<template>
+  <footer
+    v-if="$slidev.nav.currentLayout !== 'cover' && $slidev.nav.currentLayout !== 'end'"
+    class="absolute bottom-0 w-full flex justify-between p-4 text-xs"
+  >
+  <div class="opacity-75">
+      16.12.2025 | Jan Kleinmann, Marius Braun
+    </div>
+    <div class="opacity-50">
+      <SlideCurrentNo /> / <SlidesTotal />
+    </div>
+  </footer>
+</template>
